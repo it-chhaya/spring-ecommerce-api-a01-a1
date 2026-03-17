8ccb155b83e9c0d7eebbb339ecef3aa838a2f65e
chanchhaya <it.chhaya@gmail.com>
2026-03-17 15:31:01 +0700

    update

--- src/main/java/co/istad/chhaya/webmvc/dto/ProductResponse.java
+++ src/main/java/co/istad/chhaya/webmvc/dto/ProductResponse.java
@@ -1,10 +1,15 @@
 package co.istad.chhaya.webmvc.dto;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record ProductResponse(
         String code,
         String name,
-        BigDecimal price
+        BigDecimal price,
+        Boolean status,
+        String categoryName
 ) {
 }
--- src/main/java/co/istad/chhaya/webmvc/service/ProductServiceImpl.java
+++ src/main/java/co/istad/chhaya/webmvc/service/ProductServiceImpl.java
@@ -1,10 +1,12 @@
 package co.istad.chhaya.webmvc.service;
 
 import co.istad.chhaya.webmvc.domain.Category;
+import co.istad.chhaya.webmvc.domain.Product;
 import co.istad.chhaya.webmvc.dto.CreateProductRequest;
 import co.istad.chhaya.webmvc.dto.ProductResponse;
 import co.istad.chhaya.webmvc.repository.CategoryRepository;
 import co.istad.chhaya.webmvc.repository.ProductRepository;
+import co.istad.chhaya.webmvc.util.GenerateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -30,8 +32,27 @@ public class ProductServiceImpl implements ProductService {
                 .orElseThrow(() -> new RuntimeException("Category ID doesn't exist"));
         log.info("category: {}", category.getId());
 
+        // 2. Transfer data from DTO to Entity
+        Product product = new Product();
+        product.setCategory(category);
+        product.setName(createProductRequest.name());
+        product.setPrice(createProductRequest.price());
 
-        return null;
+        // 3. Set system data
+        product.setStatus(true);
+        product.setCode(GenerateUtil.generateProductCode());
+
+        // 4. Save data into database
+        product = productRepository.save(product);
+
+        // 5. Transfer data from Entity to DTO
+        return ProductResponse.builder()
+                .code(product.getCode())
+                .name(product.getName())
+                .price(product.getPrice())
+                .status(product.getStatus())
+                .categoryName(product.getCategory().getName())
+                .build();
     }
 
 }
